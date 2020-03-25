a7081fc13b89c2b98f6220cae11ad21b40ce8dd9
Charles Tison <ctison@pm.me>
2020-03-25 19:47:27 +0100

    v1.0.0

---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+use std::error::Error;
+
+pub static BASE2: &str = "01";
+pub static BASE8: &str = "01234567";
+pub static BASE10: &str = "0123456789";
+pub static BASE16: &str = "0123456789ABCDEF";
+
+fn check_base(base: &str) -> Result<(), String> {
+  if base.len() < 2 {
+    return Err(format!(
+      "base length should be at least 2: '{}'.len() -> {}",
+      base,
+      base.len()
+    ));
+  }
+  for c in base.chars() {
+    if base.chars().filter(|c2| &c == c2).count() > 1 {
+      return Err(format!(
+        "base must have unique characters: '{}' has multiple '{}'",
+        base, c,
+      ));
+    }
+  }
+  Ok(())
+}
+
+pub fn base_to_decimal(nbr: &str, from_base: &str) -> Result<usize, Box<dyn Error>> {
+  check_base(from_base)?;
+  let baselen = from_base.len();
+  let mut result: usize = 0;
+  for (c, i) in nbr.chars().zip((0..nbr.chars().count() as u32).rev()) {
+    let x = from_base
+      .chars()
+      .position(|x| x == c)
+      .ok_or_else::<String, _>(|| format!("char '{}' not found in base '{}'", c, from_base))?;
+    result += x
+      * baselen
+        .checked_pow(i)
+        .ok_or_else(|| format!("baselen {} ** {} overflowed", baselen, i))? as usize;
+  }
+  Ok(result)
+}
+
+pub fn decimal_to_base(mut x: usize, to_base: &str) -> Result<String, String> {
+  check_base(to_base)?;
+  if x == 0 {
+    return Ok(String::from("0"));
+  }
+  let baselen = to_base.chars().count();
+  let mut result = String::new();
+  while x > 0 {
+    result.push(nbr_to_char(x % baselen, to_base)?);
+    x /= baselen;
+  }
+  Ok(result.chars().rev().collect())
+}
+
+fn nbr_to_char(nbr: usize, base: &str) -> Result<char, String> {
+  base.chars().nth(nbr).ok_or_else(|| {
+    format!(
+      "number {} should be inferior of base len: '{}'.chars().count() == {}",
+      nbr,
+      base,
+      base.chars().count()
+    )
+  })
+}
+
+pub fn base_to_base(x: &str, from_base: &str, to_base: &str) -> Result<String, Box<dyn Error>> {
+  let y = base_to_decimal(x, from_base)?;
+  let z = decimal_to_base(y, to_base)?;
+  Ok(z)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_base_to_decimal() {
+    assert_eq!(51966, base_to_decimal("CAFE", BASE16).unwrap());
+    assert_eq!(42, base_to_decimal("101010", BASE2).unwrap());
+    assert_eq!(0, base_to_decimal("0", BASE8).unwrap());
+    assert_eq!(0, base_to_decimal("", BASE8).unwrap());
+  }
+
+  #[test]
+  fn test_decimal_to_base() {
+    assert_eq!("CAFE", decimal_to_base(51966, BASE16).unwrap());
+    assert_eq!("0", decimal_to_base(0, BASE8).unwrap());
+  }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate clap;
+use lib::base_to_base;
+
+fn main() {
+  let matches = clap_app!(base_converter =>
+      (version: "1.0")
+      (about: "Convert numbers from any to any base")
+      (@arg NUMBER: +required "Number to be converted from FROM_BASE to TO_BASE")
+      (@arg FROM_BASE: +required "Base in which NUMBER is converted from")
+      (@arg TO_BASE: +required "Base to which NUMBER is converted to")
+  )
+  .get_matches();
+  let number = matches.value_of("NUMBER").unwrap();
+  let from_base = matches.value_of("FROM_BASE").unwrap();
+  let to_base = matches.value_of("TO_BASE").unwrap();
+  match base_to_base(number, from_base, to_base) {
+    Ok(result) => println!("{}", result),
+    Err(err) => println!("{:?}", err),
+  }
+}
