fa26b0b6195b1f330899acd5a32f335b327ea0c8
Charles Tison <ctison@pm.me>
2020-03-26 12:19:48 +0100

    Update CLI usage

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use lib::base_to_base;
 fn main() {
   let matches = clap_app!(base =>
       (version: "1.0.0")
-      (about: "Convert numbers from any to any base")
+      (about: "Convert numbers from any base to any other base")
       (@arg NUMBER: +required "Number to be converted from FROM_BASE to TO_BASE")
       (@arg FROM_BASE: +required "Base in which NUMBER is converted from")
       (@arg TO_BASE: +required "Base to which NUMBER is converted to")
